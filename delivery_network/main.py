a44cd3ebfeb3634d0f8354d6c558d109cd91f1bd
jboudier <julien.boudier@ensae.fr>
2023-03-16 10:04:42 +0000

    Brute force Q18

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,13 +1,14 @@
 from graph import Graph, graph_from_file
 import numpy as np
 from tqdm import tqdm
+import numpy as np
 
 
 data_path = "input/"
-file_name = "network.01.in"
+# file_name = "network.01.in"
 
-g = graph_from_file(data_path + file_name)
-print(g)
+# g = graph_from_file(data_path + file_name)
+# print(g)
 
 ######################### Question 10 ############################
 
@@ -23,8 +24,8 @@ def estimate_time(filename_route, filename_network):
     # Create a list of trips
     all_trip = []
     for line in data:
-        src, dest, _ = map(int, line.split())
-        all_trip.append([src,dest])
+        src, dest, _ = map(float, line.split())
+        all_trip.append([int(src),int(dest)])
     # Choose some random trips
     nb_trip = 5
     chosen_trip = []
@@ -96,4 +97,133 @@ def estimate_time_mst(filename_route, filename_network):
 # estimate_time_mst('routes.7.in', 'network.7.in')
 # estimate_time_mst('routes.8.in', 'network.8.in')
 # estimate_time_mst('routes.9.in', 'network.9.in')
-estimate_time_mst('routes.10.in', 'network.10.in')
+# estimate_time_mst('routes.10.in', 'network.10.in')
+
+
+######################### Question 18 ############################
+
+# A brute force solution to the problem
+
+def max_profit(graph,routes,trucks):
+    """Returns the maximum profit achievable using the trucks, without budget constraint.
+
+    Args:
+        graph (Graph): graph of the network
+        routes (list): list of trips (src, dest, profit)
+        trucks (int list): list of trucks (power, cost)
+
+    Returns:
+        int: maximum_profit
+    """
+    alocated_trips = []
+    maximum_profit = 0
+
+    # Sort trips by descending profit
+    routes = sorted(routes, key=lambda x: x[2], reverse=True)
+
+    # Sort trucks by power
+    trucks = sorted(trucks)
+
+    # Compute required power for each trip
+    minimum_powers = [graph.min_power_mst(src,dest) for src, dest, _ in routes]
+    
+    # Main iteration
+    while trucks:
+        truck = trucks.pop(0)
+        for i in range(len(routes)):
+            if truck[0] >= minimum_powers[i] and i not in alocated_trips:
+                alocated_trips.append(i)
+                maximum_profit += routes[i][2]
+                break
+        
+    return maximum_profit
+    
+
+def solve(graph, routes, trucks, budget):
+    """Brute force the exact solution of the problem
+
+    Args:
+        trucks (list): list of trucks (power, cost)
+        budget (int): budget
+
+    Returns:
+        list: collection of trucks
+    """
+    combinations = [[]]
+    best_solution = []
+
+    # Initialize best profit
+    best_profit = 0
+
+    # Get every combination of trucks
+    for truck in trucks:
+        new_combination = [combination + [truck] for combination in combinations]
+        combination.extend(new_combination)
+    
+    # At least one truck is chosen
+    combinations.pop(0)
+
+    # Go through each combination of trucks to find the optimal solution
+    for combination in combinations:
+        # Compute the cost of the combination and its profit
+        sum_cost = sum([cost for _, cost in combination])
+        profit = max_profit(graph, routes, combination)
+
+        # Check if the cost doesn't exceed the budget and if the profit is larger
+        if sum_cost <= budget and profit > best_profit:
+            best_profit = profit
+            best_solution = combination
+
+    return best_solution
+
+def routes_from_file(filename_route):
+    """Make a list of routes from a file
+
+    Args:
+        filename_route (string): name of the file
+
+    Returns:
+        list: a list of routes (src, dest, profit)
+    """ 
+    routes = []
+
+    # Load the data from the route file
+    with open(data_path + filename_route, "r") as f:
+        f.readline()
+        data = f.readlines()
+    
+    for line in data:
+        src, dest, profit = map(float, line.split())
+        routes.append([int(src), int(dest), profit])
+    
+    return routes
+
+def trucks_from_file(filename_truck):
+    """Make a list of trucks from a file
+
+    Args:
+        filename_truck (string): name of the file
+
+    Returns:
+        list: a list of trucks (power, cost)
+    """ 
+    trucks = []
+
+    # Load the data from the truck file
+    with open(data_path + filename_truck, "r") as f:
+        f.readline()
+        data = f.readlines()
+    
+    for line in data:
+        power, cost = map(float, line.split())
+        trucks.append([power, cost])
+    
+    return routes
+
+g = graph_from_file(data_path + 'network.1.in')
+g = g.kruskal()
+routes = routes_from_file('routes.1.in')
+trucks = trucks_from_file('trucks.0.in')
+b = 25*10e9
+
+solve(g, routes, trucks, b)
